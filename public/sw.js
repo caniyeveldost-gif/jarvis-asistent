35f489ec73f4a7fdccb2af522f6d862fcc1d3f33
caniyeveldost-gif <caniyeveldost@gmail.com>
2026-08-17 00:38:34 +0400

    chore: switch PWA icons to PNG format
    Replace SVG icons with PNG assets for better compatibility and update cache version.

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,10 @@
-const CACHE_NAME = 'jarvis-pwa-v1';
+const CACHE_NAME = 'jarvis-pwa-v2';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
   '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png',
   '/icon-192.svg',
   '/icon-512.svg',
 ];
